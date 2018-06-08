279c7e593d67bae5dc3e029869965d6dbb4eaff6
wildtree <13306149026@163.com>
2018-06-08 15:22:25 +0800

    Add files via upload

---
+++ src/main/java/com/wildtree/Application.java
@@ -0,0 +1,26 @@
+package com.wildtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 启动类
+ * @author wildtree
+ *
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
+}
